9fb5bb229afa4cdd957846127e15bf027a0b6721
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-09-06 23:50:23 -0300

    [feat] Creating project
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

---
+++ armu/src/main/java/com/lorialawrence/armu/ArmuApplication.java
@@ -0,0 +1,13 @@
+package com.lorialawrence.armu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmuApplication.class, args);
+	}
+
+}
---
+++ armu/src/test/java/com/lorialawrence/armu/ArmuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorialawrence.armu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
